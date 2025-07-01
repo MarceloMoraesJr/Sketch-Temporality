783414a02fab72be2bc339d58c13f3db9d51f957
MarceloMoraesJr <marcelo.junior@usp.br>
2025-07-01 21:35:11 +0000

    fix: experiment 2 quickdraw number of classes

--- src/data/quickdraw.py
+++ src/data/quickdraw.py
@@ -89,7 +89,7 @@ class QuickDrawDataset(Dataset):
 
         data = []
         labels = []
-        for label, filename in enumerate(os.listdir(self.preprocessed_path)[:100]):
+        for label, filename in enumerate(os.listdir(self.preprocessed_path)):
             data.append(np.load(self.preprocessed_path.joinpath(filename), encoding='latin1', allow_pickle=True)[split])
             labels.append(np.full(len(data[-1]), label, dtype=np.int64))        
 
