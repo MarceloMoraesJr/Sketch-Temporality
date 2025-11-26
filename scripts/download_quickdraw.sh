2c52b79a79f62de9a0f27258d25da38792d26a05
MarceloMoraesJr <marcelo.junior@usp.br>
2025-11-26 18:48:28 +0000

    ready to push

--- scripts/download_quickdraw.sh
+++ scripts/download_quickdraw.sh
@@ -1,3 +1,5 @@
+#categories list from: https://github.com/leosampaio/sketchformer/blob/master/prep_data/quickdraw/list_quickdraw.txt
+
 while read p; do
   wget "https://storage.googleapis.com/quickdraw_dataset/sketchrnn/$p.npz" -P ./data/quickdraw/raw/
 done < ./scripts/categories.txt
